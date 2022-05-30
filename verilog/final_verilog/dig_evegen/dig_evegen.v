22b3b30204784e22001114e8c19b700346be27e8
Ishan Gupta <ishangupta033@gmail.com>
2022-05-30 12:14:25 -0700

    level crossing inserted in the testbench of digital generator

--- verilog/final_verilog/dig_evegen/dig_evegen.v
+++ verilog/final_verilog/dig_evegen/dig_evegen.v
@@ -47,13 +47,30 @@ module and_gate(
 	assign #1 out=in1&in2;
 endmodule
 
+module or_gate(
+	input in1,
+	input in2,
+	output wire out);
+	assign out=in1|in2;
+endmodule
+
 module tb_dig_evegen;
 	reg phi1b_dig;
-	wire eve,polxevent,comp_out,comp_high;
-	reg clkdiv2,clkdiv4,input_signal,ref;
-	and_gate ag(
+	wire eve,polxevent,comp_out,comp_high_int,comp_high_int2,comp_high;
+	reg clkdiv2,clkdiv4,input_signal,input_lc,ref,ref_lc;//input_lc refers to the waveform representing the region of level crossing overlap with the input signal.
+	and_gate ag1(
 		.in1(input_signal),
 		.in2(ref),
+		.out(comp_high_int));
+
+	and_gate ag2(
+		.in1(input_lc),
+		.in2(ref_lc),
+		.out(comp_high_int2));
+
+	or_gate or_(
+		.in1(comp_high_int),
+		.in2(comp_high_int2),
 		.out(comp_high));
 
 	dig_evegen gen(
@@ -65,12 +82,16 @@ module tb_dig_evegen;
 
 	
 	
-	parameter PERIOD_INPUT_SIGNAL=40000;			   
+	parameter PERIOD_INPUT_SIGNAL=40000;
+	parameter LEVEL_CROSSING_FACTOR=0.3; //represents the factor after which we are modelling the level crossing.			   
 	real clk_input_half_pd=PERIOD_INPUT_SIGNAL/2;
 	parameter n=1; //n is the index of core 
 	parameter PERIOD_CORE=400*(2**(n-1));
 	real clk_core_half_pd=(PERIOD_CORE)/2;
 	real clk_comp_high_half_pd=(PERIOD_CORE);
+	real lc_offset=LEVEL_CROSSING_FACTOR*PERIOD_INPUT_SIGNAL/2;
+	real lc_high=(PERIOD_INPUT_SIGNAL/2)*(1-2*LEVEL_CROSSING_FACTOR); //period until which lc input wave stays high
+	
 
 	initial begin
 	$dumpfile("dig_evegen.vcd");
@@ -79,26 +100,25 @@ module tb_dig_evegen;
 
 
 	initial begin
-		clkdiv2=1;
-		#1 clkdiv2=0;
+		
+		clkdiv2=0;
 		//#(clk_master_half_pd) clkdiv2=1;
 		forever
 			#(clk_core_half_pd)clkdiv2 = ~clkdiv2; //ext_clk generation freq=2.56 MHz
 	end
 
 	initial begin
-		clkdiv4=1;
-		#1 clkdiv4=0;
-		#(clk_core_half_pd-1)clkdiv4=1;
+		
+		clkdiv4=0;
+		#(clk_core_half_pd)clkdiv4=1;
 		//#(clk_master_half_pd) clkdiv2=1;
 		forever
 			#(clk_comp_high_half_pd)clkdiv4 = ~clkdiv4; //ext_clk generation freq=2.56 MHz
 	end
 
 	always @(posedge clkdiv2 or negedge clkdiv2) begin
-		#2;
 		if(clkdiv2) begin
-			phi1b_dig<=0;
+			#2 phi1b_dig<=0;
 			#(clk_core_half_pd-2) phi1b_dig<=1;
 		end
 		else begin
@@ -107,10 +127,9 @@ module tb_dig_evegen;
 	end
 
 	always @(posedge clkdiv4 or negedge clkdiv4) begin
-		#3;
 		if(clkdiv4) begin
-			ref<=1;
-			#(clk_core_half_pd-1) ref<=0;//+1 added because clock takes 1n for transition from 1-0 at start
+			#2 ref<=1;
+			#(clk_core_half_pd-2) ref<=0;
 		end
 		else begin
 			ref<=ref;
@@ -119,13 +138,30 @@ module tb_dig_evegen;
 
 	initial begin
 		input_signal=0;
-		#(clk_core_half_pd+1) input_signal=1; //+1 added because clock takes 1n for transition from 1-0 at start
+		#(clk_core_half_pd) input_signal=1; 
 		forever
 			#(clk_input_half_pd) input_signal=~input_signal;
 	end
-
 	
+	// lc signal modelling
 
+	always @(posedge input_signal) begin
+		input_lc<=0;
+		#(lc_offset) input_lc<=1;
+		#(lc_high) input_lc<=0; 
+	end
+
+	always @(posedge clkdiv4 or negedge clkdiv4) begin
+		if(!clkdiv4) begin
+			#2 ref_lc<=1;
+			#(clk_core_half_pd-2) ref_lc<=0;
+		end
+		else begin
+			ref_lc<=ref_lc;
+		end
+	end
+	
+		
 	initial begin
 		repeat(700) @(posedge phi1b_dig);
 		#100;
