a9095c61ab6280583720239d8ddadfa93c8c9268
Abhinav Uppal <auppal@ucsd.edu>
2022-09-09 23:16:21 -0700

    four cores in 960x100

--- verilog/final_verilog/digital_unison/digital_unison.v
+++ verilog/final_verilog/digital_unison/digital_unison.v
@@ -8,7 +8,7 @@
 `endif
 */
 
-module digital_unison #(parameter NUM_CORES=2)
+module digital_unison #(parameter NUM_CORES=4)
 (
 	// global inputs from RISC (must level shift vccd2->vccd1)
 	input rstb, ud_en,//phi1b_dig[0] is corresponding to the first wrapper
