4eee04e49b6927380e74ae3afd2f33a2b76ab953
Abhinav Uppal <auppal@ucsd.edu>
2022-09-12 03:57:41 -0700

    digital unison regenerated with updated pin order to match analog core, left side gets LA interface, right side is empty for analog_io wiring

---
+++ magic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# launch magic from repo's base path so we can keep cell addresses relative to this path:
+#cd /local_disk/fossi_cochlea/mag/final_designs
+#magic -rcfile /local_disk/fossi_cochlea/dependencies/pdks/sky130B/libs.tech/magic/sky130B.magicrc # /isn/abhinav/fossi_cochlea/openlane/$1/runs/$1/results/final/mag/$1.mag
+# simlink pointsto latest run: can use just one argument for design name
+#magic -rcfile /usr/local/share/pdk/sky130A/libs.tech/magic/sky130A.magicrc ./fossi_cochlea/openlane/peripheral_gray/runs/peripheral_gray/results/final/mag/peripheral_gray.mag
+gedit /local_disk/fossi_cochlea/openlane/$1/config.tcl &
+gedit /local_disk/fossi_cochlea/openlane/$1/pin_order.cfg &
+gedit /local_disk/fossi_cochlea/openlane/$1/runs/$1/reports/manufacturability.rpt &
+
+magic -rcfile /local_disk/fossi_cochlea/dependencies/pdks/sky130B/libs.tech/magic/sky130B.magicrc /local_disk/fossi_cochlea/openlane/$1/runs/$1/results/final/mag/$1.mag &
